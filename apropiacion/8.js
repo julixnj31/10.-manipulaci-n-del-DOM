258dcb45547a69417d015f4a52988c3ef742ac41
selena <selenamolinafigueroa26@gmail.com>
2026-05-11 18:00:17 -0500

    hice el ejercicio  del D. Closes #8

---
+++ apropiacion/8.js
@@ -0,0 +1,11 @@
+/**
+ * Implementar la función showEmptyState() para mostrar
+ * nuevamente el mensaje de “sin mensajes” cuando no existan mensajes.
+ * 
+ * Se utiliza classList.remove("hidden") para remover
+ * la clase que oculta el elemento en pantalla.
+ */
+
+function showEmptyState() {
+    emptyState.classList.remove("hidden");
+}
