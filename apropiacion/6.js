ffa6b005b804c47bbe9cd2eefc13661bb6b19f75
selena <selenamolinafigueroa26@gmail.com>
2026-05-11 17:48:48 -0500

    hice el ejercicio 6 del D. Closes #6

---
+++ apropiacion/6.js
@@ -0,0 +1,7 @@
+/**
+ * Implementar la función updateMessageCount() para actualizar
+ * el contador de mensajes en pantalla usando template literals.
+ */
+function updateMessageCount() {
+    messageCount.textContent = `${totalMessages} mensajes`;
+}       
